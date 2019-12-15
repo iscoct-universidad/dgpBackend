22393f2792de8fd8fd4344a83934d7ef49e53450
iscoct <iscoct@correo.ugr.es>
2019-12-15 23:00:03 +0100

    Cambiado el get de un usuario concreto para no tener body

--- public/api/index.php
+++ public/api/index.php
@@ -16,7 +16,10 @@ $container->set('upload_directory',__DIR__ . '/../images');
 
 AppFactory::setContainer($container);
 $app = AppFactory::create();
-$app->addRoutingMiddleware();
+$app -> addRoutingMiddleware();
+
+$conexion_bd = new gestorBD();
+
 session_start();
 
 function hasBodyJson(Request $request) {
@@ -48,47 +51,52 @@ function setHeader(Request $request, Response $response) {
 }
 
 $app -> options('/[{path:.*}]', function (Request $request, Response $response, $args) {
-	
 	return setHeader($request, $response);
 });
 
-$app->get('/api/[health]', function (Request $request, Response $response, $args) {
+$app -> get('/api/[health]', function (Request $request, Response $response, $args) {
     $response -> getBody() -> write("El servidor está corriendo");
 
     return $response;
 });
 
-$app->get('/api/usuario',function (Request $request,Response $response, $args) {
-    $conexion_bd= new gestorBD();
-    $post = $request->getBody();
-    $post=json_decode($post,true);
-    if (!is_null($post['id_usuario']) && $conexion_bd->comprobarRolAdministrador($_SESSION['id_usuario']))
-        $usuario=$conexion_bd->getUsuario($post['id_usuario']);
-    else if($_SESSION['id_usuario'] !== NULL)
-        $usuario=$conexion_bd->getUsuario($_SESSION['id_usuario']);
-    else
-    	$usuario = 'No esta seteada la sesión';
-    $response = setResponse($response,array("usuario"=>$usuario), 200);
+$app -> get('/api/usuario/{id_usuario}', function (Request $request,Response $response, $args) {
+    $id_usuario = $args["id_usuario"];
+    $sessionUser = $_SESSION['id_usuario'];
+    $isAdmin = $conexion_bd -> comprobarRolAdministrador($sessionUser);
 
-    return setHeader($request, $response);
+    if (!is_null($id_usuario) && $isAdmin) {
+        $usuario = $conexion_bd -> getUsuario($id_usuario);
+    } else if(!is_null($sessionUser)) {
+        $usuario = $conexion_bd -> getUsuario($sessionUser);
+    } else {
+    	$usuario = 'No esta seteada la sesión';
+    }
+
+    $response = setResponse($response, array("usuario"=>$usuario), 200);
+    $response = setHeader($request, $response);
+
+    return $response;
 });
 
+
+
 $app->get('/api/usuarios',function (Request $request,Response $response, $args) {
-    $usuarios=array();
-    $conexion_bd= new gestorBD();
-    $usuarios=$conexion_bd->getUsuarios();
+    $usuarios = $conexion_bd -> getUsuarios();
+
     $response = setResponse($response,array("usuarios"=>$usuarios), 200);
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+    return $response;
 });
 
 $app -> get('/api/usuario/{id}', function (Request $request, Response $response, $args) {
-	$conexion_bd = new gestorBD();
 	$isIdDefined = ! is_null($args['id']);
 	$isSuperuser = $conexion_bd -> comprobarRolAdministrador($_SESSION['id_usuario']);
-	$code = 200;
 
 	if ($isIdDefined && $isSuperuser) {
-		$usuario = $conexion_bd -> getUsuario($args['id']);
+        $usuario = $conexion_bd -> getUsuario($args['id']);
+        $code = 200;
 	} else if (! $isSuperuser) {
 		$usuario = 'You are not superuser in this system';
 		$code = 403;
@@ -98,18 +106,18 @@ $app -> get('/api/usuario/{id}', function (Request $request, Response $response,
 	}
 	
 	$response = setResponse($response, array("usuario" => $usuario), $code);
-	
-	return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+	return $response;
 });
 
 $app -> get('/api/usuarioNombre/{id}', function (Request $request, Response $response, $args) {
-	$conexion_bd = new gestorBD();
-
     $usuario=$conexion_bd->getUsuarioNombre($args['id']);
 	
 	$response = setResponse($response, array("usuario" => $usuario), 200);
-	
-	return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+	return $response;
 });
 
 $app -> post('/api/usuario', function (Request $request,Response $response, $args) {
@@ -118,27 +126,24 @@ $app -> post('/api/usuario', function (Request $request,Response $response, $arg
         $response = setResponse($response, array('description' =>'El cuerpo no contiene json'), 400);
     } 
     else {
-        $conexion_bd= new gestorBD();
         $user = new Usuario;
         $post = $request->getBody();
         $post=json_decode($post,true);
-        $user->email = $post['email'];
-        $user->password = $post['password'];
-        $usuario = $conexion_bd->identificarUsuario($user);
+        $user -> email = $post['email'];
+        $user -> password = $post['password'];
+        $usuario = $conexion_bd -> identificarUsuario($user);
+    
         if ($usuario!=false){
             $response = setResponse($response, array('id_usuario' => $_SESSION['id_usuario'],'description' => 'OK'), 200);
         }
         else
             $response = setResponse($response, array('description' => 'No pudo identificarse al usuario'), 400);
-        
-        $conexion_bd->close();
     }
 
     return setHeader($request, $response);
 });
 
 $app -> post('/api/usuario/nuevo', function (Request $request, Response $response, $args) {
-    $conexion_bd= new gestorBD();
     $uploadFiles = $request->getUploadedFiles();
 
 	if (count($uploadFiles) > 0) {
@@ -163,21 +168,21 @@ $app -> post('/api/usuario/nuevo', function (Request $request, Response $respons
 		}
     }
     $new_user= new Usuario;
-    $new_user->construct2($post['rol'],$post['nombre'], $post['apellido1'], $post['apellido2'], $post['DNI'], $post['fecha_nacimiento'], $post['localidad'],
+    $new_user -> constructFromArguments($post['rol'],$post['nombre'], $post['apellido1'], $post['apellido2'], $post['DNI'], $post['fecha_nacimiento'], $post['localidad'],
                              $post['email'], $post['telefono'], $post['aspiraciones'], $post['observaciones'],$post['password'],$imagePath,$new_gustos);
     $exito = $conexion_bd->regUsuario($new_user);
-    if ($exito)
-        $response = setResponse($response,array('description'=> 'OK'), 200);
-    else
-        $response =setResponse($response,array('description'=> 'Ya existe el usuario, hay campos obligatorios vacíos o se incluyeron gustos repetidos.'), 400);
-    $conexion_bd->close();
+    if ($exito) {
+        $response = setResponse($response, array('description'=> 'OK'), 200);
+    } else {
+        $response = setResponse($response, array('description'=> 'Ya existe el usuario, hay campos obligatorios vacíos o se incluyeron gustos repetidos.'), 400);
+    }
 
     return setHeader($request, $response);
 });
 
 $app -> post('/api/usuario/modificar', function (Request $request, Response $response, $args) {
-        $conexion_bd= new gestorBD();
         $uploadFiles = $request->getUploadedFiles();
+    
         if (count($uploadFiles) > 0) {
             $imageFile = $uploadFiles['imagen'];
     
@@ -193,6 +198,7 @@ $app -> post('/api/usuario/modificar', function (Request $request, Response $res
 
         $post=$request->getParsedBody();
         $new_gustos = array();
+
         if (array_key_exists('gustos', $post)) {
             $gustos_post = json_decode($post['gustos'],true);
             for ($i=0;$i<count($gustos_post['gustos']);$i++) {
@@ -200,95 +206,91 @@ $app -> post('/api/usuario/modificar', function (Request $request, Response $res
             }
         }
 
-        $new_user= new Usuario;
-        $new_user->construct2($post['rol'],$post['nombre'], $post['apellido1'], $post['apellido2'], $post['DNI'], $post['fecha_nacimiento'], $post['localidad'],
+        $new_user = new Usuario;
+        $new_user -> constructFromArguments($post['rol'],$post['nombre'], $post['apellido1'], $post['apellido2'], $post['DNI'], $post['fecha_nacimiento'], $post['localidad'],
                              $post['email'], $post['telefono'], $post['aspiraciones'], $post['observaciones'],$post['password'],$imagePath,$new_gustos);
-        $new_user->id=$_SESSION['id_usuario'];
+        $new_user -> id=$_SESSION['id_usuario'];
 
         $exito = $conexion_bd->updateUsuario($new_user);
 
-        if ($exito)
+        if ($exito) {
             $response = setResponse($response,array('description'=> 'OK'), 200);
-        else
-            $response =setResponse($response,array('description'=> 'No tiene permiso para modificar el usuario, hay campos obligatorios vacíos o se incluyeron gustos repetidos.'), 400);
-        $conexion_bd->close();
+        } else {
+            $response = setResponse($response,array('description'=> 'No tiene permiso para modificar el usuario, hay campos obligatorios vacíos o se incluyeron gustos repetidos.'), 400);
+        }
 
     return setHeader($request, $response);
 });
 
 $app -> delete('/api/usuario/{id}', function (Request $request, Response $response, $args) {
-    $conexion_bd= new gestorBD();
     $usuario = new Usuario;
-    $usuario->id=$args['id'];
-    $exito = $conexion_bd->deleteUsuario($usuario);
-    if ($exito)
+    $usuario->id = $args['id'];
+    $exito = $conexion_bd -> deleteUsuario($usuario);
+
+    if ($exito) {
         $response = setResponse($response, array('description'=>'Usuario eliminado correctamente'), 200);
-    else
-        $response =setResponse($response, array('description'=>'El usuario no se puede eliminar porque su id no está registrado'), 400);
-    $conexion_bd->close();
+    } else {
+        $response = setResponse($response, array('description'=>'El usuario no se puede eliminar porque su id no está registrado'), 400);
+    }
 
     return setHeader($request, $response);
 });
 
 $app -> get('/api/actividades', function (Request $request, Response $response, $args) {
-    
-    $actividades=array();
-    $conexion_bd= new gestorBD();
-    $actividades=$conexion_bd->getActividades();
+    $actividades = $conexion_bd->getActividades();
+
     $response = setResponse($response,array("actividades"=>$actividades), 200);
-    $conexion_bd->close();
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+    return $reponse;
 });
 
 $app -> get('/api/actividades/terminadas', function (Request $request, Response $response, $args) {
-    
-    $actividades=array();
-    $conexion_bd= new gestorBD();
     $actividades=$conexion_bd->getActividadesTerminadas();
+
     $response = setResponse($response,array("actividades"=>$actividades), 200);
-    $conexion_bd->close();
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+    return $response;
 });
 
 $app -> get('/api/actividades/propias', function (Request $request, Response $response, $args) {
-    
-    $actividades=array();
-    $conexion_bd= new gestorBD();
-    $actividades=$conexion_bd->getActividadesPropias();
+    $actividades = $conexion_bd->getActividadesPropias();
+
     $response = setResponse($response,array("actividades"=>$actividades), 200);
-    $conexion_bd->close();
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+    
+    return $response;
 });
 
 $app -> get('/api/actividades/usuario/{id}', function (Request $request, Response $response, $args) {
-    $actividades=array();
-    $conexion_bd= new gestorBD();
     $actividades=$conexion_bd->getActividadesUsuario($args['id']);
+
     $response = setResponse($response,array("actividades"=>$actividades), 200);
-    $conexion_bd->close();
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+    return $response;
 });
 
 $app-> get('/api/actividades/{id}', function (Request $request, Response $response, $args) {
     $actividad = new Actividad;
-    $conexion_bd= new gestorBD();
     $actividad->id_actividad=$args['id'];
     $actividad=$conexion_bd->getActividad($actividad);
     $response = setResponse($response,array('actividad'=>$actividad), 200);
-    $conexion_bd->close();
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+    return $response;
 });
 
 $app -> put('/api/actividades/apuntarse/{id}', function (Request $request, Response $response, $args) {
     $actividad = new Actividad;
-    $conexion_bd= new gestorBD();
     $actividad->id_actividad=$args['id'];
     $exito=$conexion_bd->apuntarseActividad($actividad);
+
     if ($exito)
         $response = setResponse($response, array('description'=>'OK'), 200);
     else
         $response = setResponse($response, array('description'=>'No pudo apuntarse a la actividad'), 400);
-    $conexion_bd->close();
 
     return setHeader($request, $response);
 });
@@ -297,11 +299,9 @@ $app -> post('/api/actividades', function (Request $request, Response $response,
     $uploadFiles = $request->getUploadedFiles();
 
     if (! empty($uploadFiles)) {
-        echo 'entra al if';
 		$imageFile = $uploadFiles['imagen'];
 
 		if ($imageFile->getError() === UPLOAD_ERR_OK){
-            echo 'segundo if';
 		    $imagePath = moveUploadFile($this->get('upload_directory'),$imageFile);
 		}
 		else{
@@ -314,7 +314,6 @@ $app -> post('/api/actividades', function (Request $request, Response $response,
     }
 		$post=$request->getParsedBody();
 		$actividad=new Actividad;
-		$conexion_bd= new gestorBD();
 		$actividad->nombre=$post['nombre'];
         $actividad->descripcion=$post['descripcion'];
         $actividad->tipo=$post['tipo'];
@@ -336,17 +335,18 @@ $app -> post('/api/actividades', function (Request $request, Response $response,
 		$exito=$conexion_bd->regActividad($actividad);
 		if ($exito) $response = setResponse($response, array('description'=>'OK'), 200);
 		else $response = setResponse($response, array('description'=>'No ha sido posible crear la actividad'), 400);
-		$conexion_bd->close();
     return setHeader($request, $response);
 });
 
 $app->get('/api/actividades/chat/{id}', function (Request $request, Response $response, $args) {
     $actividad = new Actividad;
-    $conexion_bd= new gestorBD();
     $actividad->id_actividad=$args['id'];
     $actividad = $conexion_bd->getChat($actividad);
+
     $response = setResponse($response,array('chat'=>$actividad->mensajes_chat),200);
-    return setHeader($request, $response);
+    $response = setHeader($request, $response);
+
+    return $response;
 });
 
 $app->post('/api/actividades/chat/{id}', function (Request $request, Response $response, $args) {
@@ -358,66 +358,19 @@ $app->post('/api/actividades/chat/{id}', function (Request $request, Response $r
         $post=$request->getBody();
         $post=json_decode($post,true);
         $mensajeChat=new MensajeChat;
-        $conexion_bd= new gestorBD();
         $mensajeChat->id_actividad=$args['id'];
         $mensajeChat->contenido=$post['contenido'];
+
         $exito=$conexion_bd->publicarMensaje($mensajeChat);
-        if ($exito)
+    
+        if ($exito) {
             $response = setResponse($response,array('description' =>'OK'), 200);
-        else
+        } else {
             $response = setResponse($response,array('description' =>'No se pudo enviar el mensaje'), 400);
-    }
-    return setHeader($request, $response);
-});
-
-/*
-$app -> put('/api/actividades/proponerFechaLocalizacion/{id}', function (Request $request, Response $response, $args) {
-    $comparison = hasBodyJson($request);
-
-    if ($comparison) {
-        $response = setReponse($response, array('description' =>'El cuerpo no contiene json'), 400);
-    } else {
-        $put=$request->getBody();
-        $put=json_decode($put,true);
-        $actividad=new Actividad;
-        $conexion_bd= new gestorBD();
-        $actividad->id_actividad=$args['id'];
-        $actividad->fecha=$put['fecha'];
-        $actividad->localizacion=$put['localizacion'];
-        $exito=$conexion_bd->proponerFechaLocalizacion($actividad);
-        if ($exito)
-            $response = setResponse($response,array('description' =>'OK'), 200);
-        else
-            $response = setResponse($response,array('description' =>'No se pudo proponer esa fecha y hora'), 400);
-    }
-
-    return setHeader($request, $response);
-});
-
-$app -> put('/api/actividades/confirmarFechaLocalizacion/{id}', function (Request $request, Response $response, $args) {
-    $comparison = hasBodyJson($request);
-
-    if ($comparison) {
-        $response = setReponse($response, array('description' =>'El cuerpo no contiene json'), 400);
-    } else {
-        $put=$request->getBody();
-        $put=json_decode($put,true);
-        $actividad=new Actividad;
-        $conexion_bd= new gestorBD();
-        $actividad->id_actividad=$args['id'];
-        $actividad->cerrada=$put['cerrada'];
-        $exito = $conexion_bd->confirmarFechaLocalizacion($actividad);
-        if ($exito){
-            $response = setResponse($response,array( 'description'=>'OK'), 200);
-        }
-        else{
-            $response = setResponse($response,array( 'description'=>'No se pudo confirmar o rechazar la fecha y localizacion'), 400);
         }
     }
-
     return setHeader($request, $response);
 });
-*/
 
 $app -> put('/api/actividades/cerrar/{id}', function (Request $request, Response $response, $args) {
     $comparison = hasBodyJson($request);
@@ -429,7 +382,6 @@ $app -> put('/api/actividades/cerrar/{id}', function (Request $request, Response
         $put=json_decode($put,true);
 
         $actividad = new Actividad;
-        $conexion_bd= new gestorBD();
         $actividad->id_actividad=$args['id'];
 
         if (empty($put['fecha']) || $put['fecha']=='') $put['fecha']=null;
@@ -437,12 +389,14 @@ $app -> put('/api/actividades/cerrar/{id}', function (Request $request, Response
         if (empty($put['localizacion']) || $put['localizacion']=='') $put['localizacion']=null;
         $actividad->localizacion=$put['localizacion'];
         $exito=$conexion_bd->cerrarActividad($actividad);
-        if ($exito)
+
+        if ($exito) {
             $response = setResponse($response, array('description'=>'OK'), 200);
-        else
+        } else {
             $response = setResponse($response, array('description'=>'No pudo cerrarse la actividad'), 400);
-        $conexion_bd->close();
+        }
     }
+
     return setHeader($request, $response);
 });
 
@@ -455,7 +409,6 @@ $app -> put('/api/actividades/valorar/{id}', function (Request $request, Respons
         $put=$request->getBody();
         $put=json_decode($put,true);
         $actividad=new Actividad;
-        $conexion_bd= new gestorBD();
         $actividad->id_actividad=$args['id'];
         $puntuacion=$put['puntuacion'];
         $texto_valoracion=$put['texto_valoracion'];
@@ -473,7 +426,6 @@ $app -> put('/api/actividades/valorar/{id}', function (Request $request, Respons
 
 $app -> get('/api/buscarUsuario/{keywords}', function (Request $request, Response $response, $args) {
     $keywords = $args['keywords'];
-    $conexion_bd= new gestorBD();
     $listaUsuarios = $conexion_bd->buscarUsuarios($keywords);
     $response = setResponse($response,array('usuarios'=>$listaUsuarios),200);
     return setHeader($request, $response);
